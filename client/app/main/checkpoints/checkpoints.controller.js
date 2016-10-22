2ae267ac2408d9ec7d0deca7f7e463d712f33675
Tomescu Mihail <Tomescu Mihail>
2016-10-22 19:40:40 +0300

    complete task button

--- client/app/main/checkpoints/checkpoints.controller.js
+++ client/app/main/checkpoints/checkpoints.controller.js
@@ -4,9 +4,14 @@ angular.module('jsHaksApp')
   .controller('CheckpointsCtrl', function ($scope, $http, socket, mainService) {
     var vm = this;
 
+    vm.completeTask = completeTask;
+
     vm.documentDetails = mainService.getLocalDetails();
     ////////
 
+    function completeTask(checkPoint) {
+        checkPoint.completed = true;
+    }
 
 
   });
