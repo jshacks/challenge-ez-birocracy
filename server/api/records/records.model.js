2acbdd48221ddce6232a68ac5c772b5597078d7a
Ancuta Barzu <ancuta.barzu@gmail.com>
2016-10-22 19:43:00 +0300

    Added models for database and create objects at init for list of records

--- server/api/records/records.model.js
+++ server/api/records/records.model.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
+
+var Documents = require('../documents/documents.model');
+var Steps = require('../steps/steps.model');
 
 var RecordsSchema = new Schema({
   name: String,
-  info: String,
-  active: Boolean
+  label: String,
+  documents: [Documents],
+  steps: [Steps]
 });
 
 module.exports = mongoose.model('Records', RecordsSchema);
