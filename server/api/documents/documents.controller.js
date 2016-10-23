9713d3d5d0a5aa275e6a1d036c2f5c475b7a072f
Ancuta Barzu <ancuta.barzu@gmail.com>
2016-10-23 12:03:40 +0300

    Modified controller for documents

--- server/api/documents/documents.controller.js
+++ server/api/documents/documents.controller.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var Documents = require('./documents.model');
 
 // Get list of documents
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Documents.find()
     .populate('location')
     .exec(function (error, documents) {
@@ -16,43 +16,63 @@ exports.index = function(req, res) {
 };
 
 // Get a single documents
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Documents.findById(req.params.id, function (err, documents) {
-    if(err) { return handleError(res, err); }
-    if(!documents) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!documents) {
+      return res.send(404);
+    }
     return res.json(documents);
   });
 };
 
 // Creates a new documents in the DB.
-exports.create = function(req, res) {
-  Documents.create(req.body, function(err, documents) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  Documents.create(req.body, function (err, documents) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, documents);
   });
 };
 
 // Updates an existing documents in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Documents.findById(req.params.id, function (err, documents) {
-    if (err) { return handleError(res, err); }
-    if(!documents) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!documents) {
+      return res.send(404);
+    }
     var updated = _.merge(documents, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, documents);
     });
   });
 };
 
 // Deletes a documents from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Documents.findById(req.params.id, function (err, documents) {
-    if(err) { return handleError(res, err); }
-    if(!documents) { return res.send(404); }
-    documents.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!documents) {
+      return res.send(404);
+    }
+    documents.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
