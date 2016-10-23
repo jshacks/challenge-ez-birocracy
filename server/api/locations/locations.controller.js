fbf12754871d0a10aceba9b8ec9326d9a4e05d6e
Ancuta Barzu <ancuta.barzu@gmail.com>
2016-10-23 12:04:01 +0300

    Modified locations controller and model

--- server/api/locations/locations.controller.js
+++ server/api/locations/locations.controller.js
@@ -4,51 +4,73 @@ var _ = require('lodash');
 var Locations = require('./locations.model');
 
 // Get list of locationss
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Locations.find(function (err, locationss) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, locationss);
   });
 };
 
 // Get a single locations
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Locations.findById(req.params.id, function (err, locations) {
-    if(err) { return handleError(res, err); }
-    if(!locations) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!locations) {
+      return res.send(404);
+    }
     return res.json(locations);
   });
 };
 
 // Creates a new locations in the DB.
-exports.create = function(req, res) {
-  Locations.create(req.body, function(err, locations) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  Locations.create(req.body, function (err, locations) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, locations);
   });
 };
 
 // Updates an existing locations in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Locations.findById(req.params.id, function (err, locations) {
-    if (err) { return handleError(res, err); }
-    if(!locations) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!locations) {
+      return res.send(404);
+    }
     var updated = _.merge(locations, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, locations);
     });
   });
 };
 
 // Deletes a locations from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Locations.findById(req.params.id, function (err, locations) {
-    if(err) { return handleError(res, err); }
-    if(!locations) { return res.send(404); }
-    locations.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!locations) {
+      return res.send(404);
+    }
+    locations.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
