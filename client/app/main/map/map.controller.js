a2979bbea7f1624f992beb2eb2f4f377b482c77a
radudobre <radu.dobre@geometry.com>
2016-10-23 13:08:05 +0300

    route generator

--- client/app/main/map/map.controller.js
+++ client/app/main/map/map.controller.js
@@ -10,7 +10,7 @@ angular.module('jsHaksApp')
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var request = {radius: '1000'};
     var docs = mainService.getLocalDetails()[0].documents;
-    var checkpoints = [];
+    vm.checkpoints = [];
 
     vm.initialize = function(){
       var map = new google.maps.Map(document.getElementById('map'), {
@@ -18,6 +18,7 @@ angular.module('jsHaksApp')
         center: new google.maps.LatLng(44.4482678, 26.0452267),
       });
 
+      directionsDisplay.setMap(map);
       startingMarker = new google.maps.Marker({
         title:"Start point"
       });
@@ -33,11 +34,11 @@ angular.module('jsHaksApp')
           map.setCenter(pos);
           startingMarker.setMap(map);
 
-          request.location =  {lat: startingMarker.getPosition().lat(), lng: startingMarker.getPosition().lng()};
+       /*   request.location =  {lat: startingMarker.getPosition().lat(), lng: startingMarker.getPosition().lng()};
           request.type = 'post_office';
 
           service = new google.maps.places.PlacesService(map);
-          service.nearbySearch(request, callback);
+          service.nearbySearch(request, callback);*/
 
         }, function() {
           handleLocationError(true, startingMarker, map.getCenter());
@@ -56,32 +57,50 @@ angular.module('jsHaksApp')
 
       google.maps.event.addListener(map, 'click', function(event) {
         startingMarker.setPosition(event.latLng);
-        checkpoints=[];
+        vm.checkpoints=[];
         var waypointType = docs[1].location[0].type;
 
         request.location =  {lat: startingMarker.getPosition().lat(), lng: startingMarker.getPosition().lng()};
         request.type = waypointType;
         service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
+        service.nearbySearch(request, function(results,status){
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            if(results.length > 0){
+              vm.checkpoints.push(results[0]);
+
+              request.location =  {lat: vm.checkpoints[0].geometry.location.lat(), lng: vm.checkpoints[0].geometry.location.lng()};
+              request.type = docs[2].location[0].type;
+              service = new google.maps.places.PlacesService(map);
+              service.nearbySearch(request, function(results,status){
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                  vm.checkpoints.push(results[0]);
+                  calculateAndDisplayRoute(directionsService,directionsDisplay, vm.checkpoints);
+                }
+              });
+            }
+          }
+        });
       });
     }
 
-    function callback(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-          if(results.length > 0){
-            checkpoints.push(results[0]);
-
-            request.location =  {lat: checkpoints[0].geometry.location.lat(), lng: checkpoints[0].geometry.location.lng()};
-            request.type = docs[2].location[0].type;
-            service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, pushCheckpoint);
-          }
-      }
-    }
-
-    function pushCheckpoint(results, status){
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        checkpoints.push(results[0]);
-      }
+    function calculateAndDisplayRoute(directionsService, directionsDisplay, checkpoints) {
+      var waypoints = [];
+      var waypoint = new google.maps.LatLng(checkpoints[0].geometry.location.lat(),checkpoints[0].geometry.location.lng());
+      waypoints.push({
+        location: waypoint,
+        stopover: true
+      });
+      directionsService.route({
+        origin: new google.maps.LatLng(startingMarker.getPosition().lat(),startingMarker.getPosition().lng()),
+        waypoints: waypoints,
+        destination: new google.maps.LatLng(checkpoints[1].geometry.location.lat(), checkpoints[1].geometry.location.lng()),
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     }
   });
